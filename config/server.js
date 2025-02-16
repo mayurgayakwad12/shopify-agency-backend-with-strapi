d1b630026071f0f32c4de0b2e2f5f60ac99f7838
Mayur Gayakwad <mayurnamu11@gmail.com>
2025-02-16 15:00:10 +0530

    Fix: Added url in server.js for external access

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('RENDER_EXTERNAL_URL', 'https://backend-with-strapi-prjd.onrender.com'),
   app: {
     keys: env.array('APP_KEYS'),
   },
